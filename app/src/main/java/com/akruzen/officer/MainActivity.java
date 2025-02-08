e4a893e35d83b26ef1368e23f1e4763db2567d5f
Akruzen <1omkarphadke@gmail.com>
2025-02-08 22:17:58 +0530

    Added basic UI

--- app/src/main/java/com/akruzen/officer/MainActivity.java
+++ app/src/main/java/com/akruzen/officer/MainActivity.java
@@ -2,17 +2,35 @@ package com.akruzen.officer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.view.WindowManager;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void onAccessibilityButtonPress(View view) {
+        Intent intent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+        startActivity(intent);
+    }
+
+    public void onDeviceAdminButtonPress(View view) {
+        startActivity(new Intent().setComponent(
+                new ComponentName("com.android.settings", "com.android.settings.DeviceAdminSettings")));
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void isAllPermissionsGranted() {
+
     }
 }
