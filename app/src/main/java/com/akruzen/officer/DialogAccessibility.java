eadb9f852f998de2d5b5c688e54e1fd543e645e7
Akruzen <1omkarphadke@gmail.com>
2025-02-08 20:32:51 +0530

    Initial commit

---
+++ app/src/main/java/com/akruzen/officer/DialogAccessibility.java
@@ -0,0 +1,87 @@
+package com.akruzen.officer;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.app.KeyguardManager;
+import android.app.admin.DevicePolicyManager;
+import android.content.Context;
+import android.content.Intent;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityNodeInfo;
+
+public class DialogAccessibility extends AccessibilityService {
+
+    private void traverseNodes(AccessibilityNodeInfo node) {
+        if (node == null) return;
+
+        Log.d("AccessibilityService", "Node text: " + node.getText());
+
+        for (int i = 0; i < node.getChildCount(); i++) {
+            traverseNodes(node.getChild(i));
+        }
+    }
+
+    @Override
+    protected void onServiceConnected() {
+        super.onServiceConnected();
+        Log.d("AccessibilityService", "Service is connected and running!");
+        AccessibilityServiceInfo info = new AccessibilityServiceInfo();
+        // Set the type of events that this service wants to listen to. Others
+        // aren't passed to this service.
+        info.eventTypes = AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED |
+                AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED;
+
+        // Set the type of feedback your service provides.
+        info.feedbackType = AccessibilityServiceInfo.FEEDBACK_ALL_MASK;
+
+        // Default services are invoked only if no package-specific services are
+        // present for the type of AccessibilityEvent generated. This service is
+        // app-specific, so the flag isn't necessary. For a general-purpose service,
+        // consider setting the DEFAULT flag.
+
+        info.flags = AccessibilityServiceInfo.DEFAULT;
+
+        info.notificationTimeout = 100;
+
+        this.setServiceInfo(info);
+    }
+
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+        // Log.d("AccessibilityService", "onAccessibilityEvent: " + event.getEventType());
+        if (event.getEventType() == AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED) {
+            String packageName = event.getPackageName().toString();
+
+            if (packageName.equals("com.android.systemui")) {
+                Log.d("AccessibilityService", "System UI detected with event: " + event);
+                DevicePolicyManager devicePolicyManager = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+                boolean isPowerMenuFlag1 = event.toString().contains("ClassName: com.android.systemui.globalactions.GlobalActionsDialogLite$ActionsDialogLite");
+                boolean isPowerMenuFlag2 = event.toString().contains("FullScreen: true");
+                // Check if device screen is locked
+                boolean isScreenLocked = ((KeyguardManager) getSystemService(Context.KEYGUARD_SERVICE)).inKeyguardRestrictedInputMode();
+                if (isPowerMenuFlag1 && isPowerMenuFlag2 && isScreenLocked) {
+                    // Lock the device screen
+                    try {
+                        devicePolicyManager.lockNow();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    Log.i("AccessibilityService", "Device locked");
+                }
+            }
+
+            // Retrieve window content
+            AccessibilityNodeInfo rootNode = getRootInActiveWindow();
+            if (rootNode != null) {
+                traverseNodes(rootNode);
+            }
+        }
+    }
+
+    @Override
+    public void onInterrupt() {
+        Log.d("AccessibilityService", "Service interrupted");
+    }
+}
---
+++ app/src/main/java/com/akruzen/officer/MainActivity.java
@@ -0,0 +1,18 @@
+package com.akruzen.officer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
